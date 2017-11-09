70d2d194aa36e5f042a54764e32446a5e5651e4f
M-Aranda <cheloz_20@hotmail.com>
2017-11-09 05:46:06 -0300

    primera subida con mensaje

---
+++ src/Mensaje_inicial/Mensaje_de_inicio.java
@@ -0,0 +1,12 @@
+
+package Mensaje_inicial;
+
+
+public class Mensaje_de_inicio {
+    
+    public void mostrarMensajeDeInicio(){
+        System.out.println("Hola, bienvenido al sistema.");
+        System.out.println("Por favor ingrese 2 numeros enteros separados por una coma.");
+    }
+    
+}
---
+++ src/trabajo_poo_alg_marcelo_aranda/Trabajo_poo_alg_marcelo_aranda.java
@@ -0,0 +1,16 @@
+
+package trabajo_poo_alg_marcelo_aranda;
+
+import Mensaje_inicial.Mensaje_de_inicio;
+
+public class Trabajo_poo_alg_marcelo_aranda {
+
+
+    public static void main(String[] args) {
+      
+        Mensaje_de_inicio mensaje=new Mensaje_de_inicio();
+        mensaje.mostrarMensajeDeInicio();
+        
+    }
+    
+}
